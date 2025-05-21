21f96d0b834f74cb340d0373abec30a695b80526
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-05-21 09:48:11 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CardList from './components/CardList';
+import './style.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CardList />
+  </React.StrictMode>,
+);
