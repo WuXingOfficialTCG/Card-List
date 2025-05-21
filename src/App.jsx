64ae5d9d48587fe8585749464b5506e25fe9b6ca
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-05-21 22:58:13 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import CardGrid from './components/CardGrid';
+import Popup from './components/Popup';
 
 export default function App() {
   return (
