99ad913a936b76988a114d563f2535f212a5e1e8
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-05-21 12:11:07 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,44 +16,23 @@ export default function Header({ onSupportClick }) {
 
       {/* Zona centrale */}
       <div className="header-center">
-        <h1 className="logo">
-          <FaHeart className="inline-icon" /> Wu Xing TCG
-        </h1>
+        <img src="https://i.imgur.com/TUO_LOGO.png" alt="Logo" className="logo-img" />
+        <h1 className="logo-text">Wu Xing TCG</h1>
       </div>
 
       {/* Zona destra */}
       <nav className="header-right social-links">
-        <a
-          href="https://discord.gg/tuo-invito"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="Discord"
-        >
-          <FaDiscord />
+        <a href="https://discord.gg/tuo-invito" target="_blank" rel="noopener noreferrer" title="Discord">
+          <img src="https://i.imgur.com/discord-icon.png" alt="Discord" className="social-icon" />
         </a>
-        <a
-          href="https://instagram.com/tuoprofilo"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="Instagram"
-        >
-          <FaInstagram />
+        <a href="https://instagram.com/tuoprofilo" target="_blank" rel="noopener noreferrer" title="Instagram">
+          <img src="https://i.imgur.com/instagram-icon.png" alt="Instagram" className="social-icon" />
         </a>
-        <a
-          href="https://facebook.com/tuoprofilo"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="Facebook"
-        >
-          <FaFacebook />
+        <a href="https://facebook.com/tuoprofilo" target="_blank" rel="noopener noreferrer" title="Facebook">
+          <img src="https://i.imgur.com/facebook-icon.png" alt="Facebook" className="social-icon" />
         </a>
-        <a
-          href="https://youtube.com/tuoprofilo"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="YouTube"
-        >
-          <FaYoutube />
+        <a href="https://youtube.com/tuoprofilo" target="_blank" rel="noopener noreferrer" title="YouTube">
+          <img src="https://i.imgur.com/youtube-icon.png" alt="YouTube" className="social-icon" />
         </a>
       </nav>
     </header>
