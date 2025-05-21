a618697abd7500c98f8235de954aff095149021c
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-05-21 22:48:22 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Header from './components/Header';
-import Main from './main.jsx';
 
 export default function App() {
   return (
     <>
       <Header />
-      <Main />
     </>
   );
 }
