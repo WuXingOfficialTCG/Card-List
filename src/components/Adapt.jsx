1fe5174758193576bdced1ea3d23237fe4d16302
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-05-24 23:27:44 +0200

    Update Adapt.jsx

--- src/components/Adapt.jsx
+++ src/components/Adapt.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 
 export default function Adapt({ children }) {
-  const [isAdaptCollapsed, setIsAdaptCollapsed] = useState(false);
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
 
-  const toggleAdaptCollapse = () => setIsAdaptCollapsed(prev => !prev);
+  const toggleSidebarCollapsed = () => setIsSidebarCollapsed(prev => !prev);
 
   return (
     <>
       {typeof children === 'function'
-        ? children({ isAdaptCollapsed, toggleAdaptCollapse })
+        ? children({ isSidebarCollapsed, toggleSidebarCollapsed })
         : null}
     </>
   );
