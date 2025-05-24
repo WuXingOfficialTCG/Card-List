d893aba776f7298e11e521a24ad1d30d22c3916a
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-05-24 22:04:42 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-// src/App.jsx
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DeckBuilder from './pages/DeckBuilder';
-// import DecksPage from './pages/DecksPage';  <-- dopo lo facciamo
+// import DecksPage from './pages/DecksPage'; // <-- verrà aggiunto in seguito
+import FloatingMenu from './components/FloatingMenu'; // importa il menu
 
 export default function App() {
   return (
     <Router>
+      <FloatingMenu /> {/* Menu sempre visibile sopra tutte le pagine */}
       <Routes>
         <Route path="/" element={<DeckBuilder />} />
         {/* <Route path="/decks" element={<DecksPage />} /> */}
