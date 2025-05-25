89f4b5866b319df1b8aad1320e3514a7dfca598c
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-05-25 22:51:08 +0200

    Update CardGrid.jsx

--- src/components/CardGrid/CardGrid.jsx
+++ src/components/CardGrid/CardGrid.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TiltCard from '../path/to/TiltCard';  // aggiorna il percorso corretto
 import './cardgrid.css';
 
 export default function CardGrid({ cards, onCardClick }) {
@@ -13,7 +14,7 @@ export default function CardGrid({ cards, onCardClick }) {
       ) : (
         <div className="card-grid">
           {cards.map((card) => (
-            <img
+            <TiltCard
               key={card.id}
               src={card.immagine}
               alt={card.nome}
