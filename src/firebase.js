5e36e68ea8624541e8c79747a861f43d8bd6aa33
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-05-26 12:45:46 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,4 @@
-import { doc, setDoc } from "firebase/firestore";
-import { auth, db } from "./firebaseConfig"; // usa il tuo file firebaseConfig
+// src/firebase.js
+import { auth, db } from './firebaseConfig';
 
-const handleRegister = async () => {
-  setError('');
-  try {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    const user = userCredential.user;
-
-    // Salvataggio dati extra su Firestore
-    await setDoc(doc(db, "users", user.uid), {
-      email: email,
-      promosConsent: promosConsent,
-      createdAt: new Date()
-    });
-
-    onSuccess();
-  } catch (err) {
-    setError(err.message);
-  }
-};
+export { auth, db };
