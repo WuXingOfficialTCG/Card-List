80c1fe765a20f353ed715edf722585f079756c0f
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-05-26 09:01:06 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,21 @@
-// src/firebaseConfig.js
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
+import { auth, db } from "./firebaseConfig"; // usa il tuo file firebaseConfig
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAFf_0n74hGJWEN1-e1KhSPffSci7pJhi8",
-  authDomain: "wu-xing-cards-6dfa6.firebaseapp.com",
-  projectId: "wu-xing-cards-6dfa6",
-  storageBucket: "wu-xing-cards-6dfa6.firebasestorage.app",
-  messagingSenderId: "124161312980",
-  appId: "1:124161312980:web:8589a5474727d973ca5341",
-  measurementId: "G-NN9WE59FQR"
+const handleRegister = async () => {
+  setError('');
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+
+    // Salvataggio dati extra su Firestore
+    await setDoc(doc(db, "users", user.uid), {
+      email: email,
+      promosConsent: promosConsent,
+      createdAt: new Date()
+    });
+
+    onSuccess();
+  } catch (err) {
+    setError(err.message);
+  }
 };
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
