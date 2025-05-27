410ed594a28c35be8aaaec84f5476e8310c682a0
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-05-27 14:42:06 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import './components/home/Home.css';
+import Header from '../components/Header/Header';
+import '../components/home/Home.css';
 
 export default function Home() {
   return (
-    <main className="home-main">
-      <h2 className="home-title">Benvenuto nel mondo di NomeDelTuoTCG!</h2>
-      <p className="home-subtitle">
-        Scopri il gioco di carte più avvincente e strategico. Sfida i tuoi amici, costruisci il tuo mazzo e diventa il campione!
-      </p>
+    <>
+      <Header />
+      <main className="home-main">
+        <h2 className="home-title">Benvenuto nel mondo di NomeDelTuoTCG!</h2>
+        <p className="home-subtitle">
+          Scopri il gioco di carte più avvincente e strategico. Sfida i tuoi amici, costruisci il tuo mazzo e diventa il campione!
+        </p>
 
-      <div className="home-button-container">
-        <a href="/deck-builder" className="home-button">Card List</a>
-        <a href="/account" className="home-button">Area Personale</a>
-      </div>
-    </main>
+        <div className="home-button-container">
+          <a href="/deck-builder" className="home-button">Card List</a>
+          <a href="/account" className="home-button">Area Personale</a>
+        </div>
+      </main>
+    </>
   );
 }
