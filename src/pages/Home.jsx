cc6ac4781969b3b5a91f19cde645b4b27baa6670
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-05-27 14:44:55 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header/Header';
-import '../components/home/Home.css';
+import '../components/home/home.css';
 
 export default function Home() {
   return (
