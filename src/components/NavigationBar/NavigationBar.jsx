b2316a6d8cc4576ef8f0acef266c792d7a634ace
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-05-27 14:52:15 +0200

    Create NavigationBar.jsx

---
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavigationBar.css';
+
+export default function NavigationBar() {
+  return (
+    <nav className="navigation-bar">
+      <Link to="/deck-builder" className="nav-button">Card List</Link>
+      <Link to="/shop" className="nav-button">Shop</Link>
+      <Link to="/events" className="nav-button">Events</Link>
+      <Link to="/account" className="nav-button">Account</Link>
+    </nav>
+  );
+}
