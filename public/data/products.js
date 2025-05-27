b2bd2cdbe517eff7c02c65b10ef468291eaa0c97
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-05-27 22:50:29 +0200

    Create products.js

---
+++ public/data/products.js
@@ -0,0 +1,34 @@
+[
+  {
+    "id": 1,
+    "name": "Prodotto A",
+    "description": "Ottimo prodotto A con caratteristiche speciali.",
+    "price": 29.99,
+    "image": "/images/prodotto-a.jpg",
+    "preorder": false
+  },
+  {
+    "id": 2,
+    "name": "Prodotto B",
+    "description": "Fantastico prodotto B, in preordine.",
+    "price": 59.99,
+    "image": "/images/prodotto-b.jpg",
+    "preorder": true
+  },
+  {
+    "id": 3,
+    "name": "Prodotto C",
+    "description": "Economico prodotto C, ottimo per tutti.",
+    "price": 15.5,
+    "image": "/images/prodotto-c.jpg",
+    "preorder": false
+  },
+  {
+    "id": 4,
+    "name": "Prodotto D",
+    "description": "Top gamma prodotto D, qualità superiore.",
+    "price": 99.0,
+    "image": "/images/prodotto-d.jpg",
+    "preorder": true
+  }
+]
