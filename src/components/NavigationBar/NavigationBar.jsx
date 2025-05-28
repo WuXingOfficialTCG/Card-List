d04b3c9ad4097603c078e6b995fa1d46de971acd
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-05-28 14:33:39 +0200

    Update NavigationBar.jsx

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,16 +1,22 @@
 // src/components/NavigationBar.jsx
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './NavigationBar.css';
 
 export default function NavigationBar() {
+  const location = useLocation();
+
+  const getClass = (path) =>
+    location.pathname === path ? 'nav-button active' : 'nav-button';
+
   return (
     <nav className="navigation-bar">
-      <Link to="/deck-builder" className="nav-button">Card List</Link>
-      <Link to="/shop" className="nav-button">Shop</Link>
-      <Link to="/events" className="nav-button">Events</Link>
-      <Link to="/account" className="nav-button">Account</Link>
-      <Link to="/admin" className="nav-button">Admin</Link>
+      <Link to="/" className={getClass('/')}>Home</Link>
+      <Link to="/deck-builder" className={getClass('/deck-builder')}>Card List</Link>
+      <Link to="/shop" className={getClass('/shop')}>Shop</Link>
+      <Link to="/events" className={getClass('/events')}>Events</Link>
+      <Link to="/account" className={getClass('/account')}>Account</Link>
+      <Link to="/admin" className={getClass('/admin')}>Admin</Link>
     </nav>
   );
 }
