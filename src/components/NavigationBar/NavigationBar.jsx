0980f52986ab62f8959e5b74490f04801fac7c17
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-05-28 11:10:01 +0200

    Update NavigationBar.jsx

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,3 +1,4 @@
+// src/components/NavigationBar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavigationBar.css';
@@ -9,6 +10,7 @@ export default function NavigationBar() {
       <Link to="/shop" className="nav-button">Shop</Link>
       <Link to="/events" className="nav-button">Events</Link>
       <Link to="/account" className="nav-button">Account</Link>
+      <Link to="/admin" className="nav-button">Admin</Link>
     </nav>
   );
 }
