d68f01c26c07b781cb968070568805f2a44dc6ba
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-06-05 23:02:10 +0200

    Update CardGrid.jsx

--- src/components/CardGrid/CardGrid.jsx
+++ src/components/CardGrid/CardGrid.jsx
@@ -1,4 +1,3 @@
-// CardGrid.jsx
 import React from 'react';
 import './cardgrid.css';
 import './cardgridResponsive.css';
@@ -6,6 +5,28 @@ import './cardgridResponsive.css';
 export default function CardGrid({ cards, onCardClick, showItalian }) {
   const handleDragStart = (e, card) => {
     e.dataTransfer.setData('application/json', JSON.stringify(card));
+  };
+
+  const handleMouseMove = (e) => {
+    const card = e.currentTarget;
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const tiltX = ((y - centerY) / centerY) * 15;
+    const tiltY = ((centerX - x) / centerX) * 15;
+
+    card.style.setProperty('--tiltX', `${tiltX}deg`);
+    card.style.setProperty('--tiltY', `${tiltY}deg`);
+  };
+
+  const handleMouseLeave = (e) => {
+    const card = e.currentTarget;
+    card.style.setProperty('--tiltX', '0deg');
+    card.style.setProperty('--tiltY', '0deg');
   };
 
   return (
@@ -23,6 +44,8 @@ export default function CardGrid({ cards, onCardClick, showItalian }) {
               onClick={() => onCardClick(card)}
               draggable
               onDragStart={(e) => handleDragStart(e, card)}
+              onMouseMove={handleMouseMove}
+              onMouseLeave={handleMouseLeave}
             />
           ))}
         </div>
