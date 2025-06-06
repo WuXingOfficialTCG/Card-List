0bbe359ae19361678324a208fe9bb3393abc8bc7
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-06-06 21:51:33 +0200

    Create ChapterEffects.jsx

---
+++ src/components/rulebook/chapters/ChapterEffects.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ChapterEffects = ({ language }) => {
+  return (
+    <div id="chapter-0" className="chapter">
+      <h2>{language === 'ita' ? 'Effetti' : 'Effects'}</h2>
+      <p>
+        {language === 'ita'
+          ? `Sempre caro mi fu quest’ermo colle, e questa siepe...`
+          : `In the middle of the journey of our life I found myself in a dark wood...`}
+      </p>
+    </div>
+  );
+};
+
+export default ChapterEffects;
