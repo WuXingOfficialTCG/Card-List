5e538d66527ee22e448e99b51380b6607ee0d480
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-06-06 21:43:14 +0200

    Create RulebookSidebar.jsx

---
+++ src/components/rulebook/RulebookSidebar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Rulebook.css';
+
+const labels = {
+  ita: [
+    'Introduzione', 'Setup', 'Carte', 'Campo di gioco', 'Effetti',
+    'Fasi del turno', 'Qi', 'Combattimento', 'Glossario'
+  ],
+  eng: [
+    'Introduction', 'Setup', 'Cards', 'Playfield', 'Effects',
+    'Turn Phases', 'Qi', 'Combat', 'Glossary'
+  ]
+};
+
+const RulebookSidebar = ({ language }) => {
+  return (
+    <div className="rulebook-sidebar">
+      <ul>
+        {labels[language].map((label, index) => (
+          <li key={index}>
+            <a href={`#chapter-${index}`}>{label}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RulebookSidebar;
