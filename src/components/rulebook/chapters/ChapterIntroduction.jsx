1fadd368a694f353d7f9c92d49ad9224a9306cd4
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-06-08 16:49:59 +0200

    Update ChapterIntroduction.jsx

--- src/components/rulebook/chapters/ChapterIntroduction.jsx
+++ src/components/rulebook/chapters/ChapterIntroduction.jsx
@@ -6,8 +6,18 @@ const ChapterIntroduction = ({ language }) => {
       <h2>{language === 'ita' ? 'Introduzione' : 'Introduction'}</h2>
       <p>
         {language === 'ita'
-          ? `Sempre caro mi fu quest’ermo colle, e questa siepe...`
-          : `In the middle of the journey of our life I found myself in a dark wood...`}
+          ? `Nel mondo degli Invocatori, il potere è nelle tue mani!
+             Invocate creature leggendarie, scatenate la magia arcana e dominate il campo di battaglia con strategie uniche. Ogni carta è un frammento di energia antica, pronto a rispondere ai vostri comandi.
+             Solo i più astuti e potenti saliranno tra i Maestri dell'Invocazione. Siete pronti a sfidare il vostro destino?
+
+             Per giocare a Wu Xing, tutto ciò di cui avete bisogno è un mazzo di carte interamente scelto da voi e alcune pedine per tenere traccia del vostro Qi.
+             Potete costruire liberamente il vostro mazzo utilizzando tutte le carte che desiderate, sia dagli Starter Deck (perfetti per i principianti) che dagli Expansion Pack per strategie ancora più avanzate!`
+          : `In the world of Invokers, power is in your hands!
+             Invoke legendary creatures, unleash arcane magic, and dominate the battlefield with unique strategies. Each card is a fragment of ancient energy, ready to respond to your command.
+             Only the most cunning and powerful will ascend among the Masters of Invoking. Are you ready to challenge your destiny?
+
+             To play Wu Xing, all you need is a deck of cards entirely chosen by you and a few tokens to keep track of your Qi.
+             You can freely build your deck using any cards you want, whether from Starter Decks (perfect for beginners) or Expansion Packs for even more advanced strategies!`}
       </p>
     </div>
   );
