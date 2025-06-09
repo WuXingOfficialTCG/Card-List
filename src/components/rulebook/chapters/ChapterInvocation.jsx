ebc9c00d986f2e69e4ebd777ec461898678894f3
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-06-09 10:30:38 +0200

    Update ChapterInvocation.jsx

--- src/components/rulebook/chapters/ChapterInvocation.jsx
+++ src/components/rulebook/chapters/ChapterInvocation.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
 
+const ChapterInvocation = ({ language }) => {
+  return (
+    <div id="chapter-0" className="chapter">
+      <h2>{language === 'ita' ? 'Invocazione' : 'Invocation'}</h2>
+      <p>
+        {language === 'ita'
+          ? `Sempre caro mi fu quest’ermo colle, e questa siepe...`
+          : `In the middle of the journey of our life I found myself in a dark wood...`}
+      </p>
+    </div>
+  );
+};
+
+export default ChapterInvocation;
