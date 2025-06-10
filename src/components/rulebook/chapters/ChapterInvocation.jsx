c113a4c0c56dee9d464dfb33df6f29d69c2ffaf9
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-06-10 08:50:44 +0200

    Update ChapterInvocation.jsx

--- src/components/rulebook/chapters/ChapterInvocation.jsx
+++ src/components/rulebook/chapters/ChapterInvocation.jsx
@@ -7,16 +7,16 @@ const ChapterInvocation = ({ language }) => {
 
       <p>
         {language === 'ita'
-          ? `Per schierare un’Entità sul campo, devi prima invocarla. Invocare è semplice: per invocare un’Entità devi sacrificare 1 Qi del suo Elemento corrispondente. 
+          ? `Per schierare un’Entità dalla tua mano sul campo, devi prima invocarla. Invocare è semplice: per invocare un’Entità devi sacrificare 1 Qi del suo Elemento corrispondente. 
           Ad esempio: per invocare un’Entità di Terra, devi sacrificare 1 Qi di Terra.`
-          : `To deploy an Entity onto the field, you must first invoke it. Invocation is simple: to invoke an Entity, you must sacrifice 1 Qi of its corresponding Element.
+          : `To deploy an Entity from your hand onto the field, you must first invoke it. Invocation is simple: to invoke an Entity, you must sacrifice 1 Qi of its corresponding Element.
           For example: to invoke an Earth Element Entity, you must sacrifice 1 Earth Qi.`}
       </p>
 
       <p>
         {language === 'ita'
-          ? `Puoi evocare Entità in due posizioni: attacco o difesa, ma sempre a faccia scoperta. La posizione delle Entità può essere cambiata una volta per turno, durante la Main Phase o la Recovery Phase.`
-          : `You can summon entities in two positions: attack or defense, but always face-up. Entity positions can be changed once per turn, during the Main Phase or the Recovery Phase.`}
+          ? `Puoi evocare Entità in due posizioni: attacco o difesa, ma sempre a faccia scoperta. La posizione delle Entità può essere cambiata una volta per turno, durante o la Main Phase o la Recovery Phase.`
+          : `You can summon entities in two positions: attack or defense, but always face-up. Entity positions can be changed once per turn, during either the Main Phase or the Recovery Phase.`}
       </p>
 
 <div
@@ -96,13 +96,10 @@ const ChapterInvocation = ({ language }) => {
 
       <p>
         {language === 'ita'
-          ? `Tuttavia, ci sono delle regole da seguire.`
-          : `However, there are some rules to follow.`}
+          ? `Tuttavia, ci sono delle regole da seguire, basato sul rango dell'entità:`
+          : `However, there are some rules to follow, based on the Entity Rank:`}
       </p>
 
-<p>
-  <strong>{language === 'ita' ? 'Rango delle Entità:' : 'Entity Rank:'}</strong>
-</p>
 <ul style={{ marginTop: 0 }}>
   <li>
     {language === 'ita'
@@ -115,47 +112,16 @@ const ChapterInvocation = ({ language }) => {
       : 'Higher Rank Entities (Rank 2 and 3) must be placed on top of a lower Rank Entity.'}
   </li>
   <li style={{ listStyleType: '"◦ "', marginLeft: '1.2em' }}>
-    Rank 1 → Rank 2 → Rank 3
+    Rank 3 → Rank 2 → Rank 1
   </li>
 </ul>
 
 
       <p>
         {language === 'ita'
-          ? `Esiste anche un altro tipo di invocazione chiamata Invocazione Rapida.`
-          : `There is also another type of invocation known as Quick Invocation.`}
-      </p>
-
-      <p>
-  {language === 'ita'
-    ? `Questa ti permette di invocare un’Entità dalla mano anche in fasi in cui normalmente non potresti (come durante la Battle Phase o il turno dell’avversario).`
-    : `This allows you to Invoke an Entity from your hand during phases when you normally wouldn't be able to (such as the battle phase or your opponent's turn).`}
-</p>
-<p>
-  {language === 'ita'
-    ? `Tuttavia, devi pagare un costo aggiuntivo:`
-    : `However, you must pay an extra cost:`}
-</p>
-<ul style={{ listStyleType: 'disc' }}>
-  <li>
-    {language === 'ita'
-      ? `Devi sacrificare 1 Qi del suo Elemento + 1 Qi dell’Elemento dell’Entità su cui vuoi impilarla.`
-      : `You must sacrifice 1 Qi of its own element + 1 Qi of the element of the Entity you want to stack it on.`}
-  </li>
-  <li>
-    {language === 'ita'
-      ? `Se vuoi impilarla su un’Entità già impilata, devi sacrificare 1 Qi del suo Elemento + 1 Qi per ciascuna Entità sottostante.`
-      : `If you want to stack your Entity on an already stacked Entity, you must sacrifice 1 Qi of its own element + 1 Qi of the element of each underlying Entity.`}
-  </li>
-</ul>
-
-
-      <p>
-        {language === 'ita'
-          ? `Inoltre ci sono alcune limitazioni: se la carta su cui vuoi invocare è bersaglio di un effetto, non puoi farlo. 
-          Ricorda: se un’Entità impilata viene distrutta, anche tutte quelle sotto vengono mandate nel Diyu.` 
-          : `Furthermore there are a couple of limitations: if the card you’re going to invoke your entity on top is target by an effect, you cannot do it. 
-          Remember: if a stacked Entity is destroyed, all underlying Entities are also sent to Diyu.`}
+          ? `Ricorda: se un'Entità sovrapposta viene distrutta, anche tutte le Entità sottostanti vengono inviate a Diyu. Questo non si applica agli effetti di “Ritorno” o “Ritiro”: in questi casi viene colpita solo l'Entità più in alto.` 
+          : `Remember: if a stacked Entity is destroyed, all underlying Entities are also sent to Diyu. This doesn’t apply to “Return” or “Withdraw” effects: in those cases only the topmost Entity is affected.
+ `}
       </p>
     </div>
   );
