cac9909a50e36e1cebb8d8adfba115ba78817607
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-06-11 14:49:11 +0200

    Update ChapterEffects.jsx

--- src/components/rulebook/chapters/ChapterEffects.jsx
+++ src/components/rulebook/chapters/ChapterEffects.jsx
@@ -1,127 +1,135 @@
 import React from 'react';
+import styles from '../Rulebook.module.css';
 
 const ChapterEffects = ({ language }) => {
   return (
     <div id="chapter-0" className="chapter">
-      <h2>{language === 'ita' ? 'Effetti' : 'Effects'}</h2>
+      <h2>{language === 'ita' ? <strong>Effetti</strong> : <strong>Effects</strong>}</h2>
 
       <p>
         {language === 'ita'
-          ? 'Puoi attivare un solo effetto per ogni carta per turno. Questo significa che non puoi attivare lo stesso effetto su 2 copie della stessa carta nello stesso turno.'
-          : 'You may activate one effect per one card per turn. That means that you cannot activate the same effect on 2 same cards in the same turn.'}
+          ? <>Puoi attivare <strong>un solo effetto per ogni carta per turno</strong>. Questo significa che non puoi attivare lo stesso effetto su 2 copie della stessa carta nello stesso turno.</>
+          : <>You may activate <strong>one effect per one card per turn</strong>. That means that you cannot activate the same effect on 2 same cards in the same turn.</>}
       </p>
 
       <p>
         {language === 'ita'
-          ? 'Gli effetti sono gratuiti da attivare e ciascuno ha una parola chiave specifica per il suo tipo:'
-          : 'Effects are free to activate and they all have a keyword specific for their type:'}
+          ? <>Gli effetti sono gratuiti da attivare e ciascuno ha una <strong>parola chiave</strong> specifica per il suo tipo:</>
+          : <>Effects are free to activate and they all have a <strong>keyword</strong> specific for their type:</>}
       </p>
 
       <ul style={{ listStyleType: 'disc', marginLeft: '1.2rem', paddingLeft: 0 }}>
         <li>
           <strong>Skill:</strong>{' '}
           {language === 'ita'
-            ? 'effetto attivo che può essere attivato solo durante il tuo turno.'
-            : 'active type of effects that can be activated only during your turn.'}
+            ? <><strong>effetto attivo</strong> che può essere attivato solo durante il tuo turno.</>
+            : <><strong>active type of effects</strong> that can be activated only <strong>during your turn</strong>.</>}
         </li>
         <li>
           <strong>Transmutation:</strong>{' '}
           {language === 'ita'
-            ? 'effetto attivo che ruota intorno alla trasmutazione di un elemento in un altro e talvolta include effetti secondari. Attivabile solo durante il tuo turno.'
-            : 'active type of effects that revolve around transmuting an element to another and, sometimes, with secondary effects. Activable only during your turn.'}
+            ? <>effetto attivo che ruota intorno alla trasmutazione di un elemento in un altro e talvolta include effetti secondari. <strong>Attivabile solo durante il tuo turno</strong>.</>
+            : <>active type of effects that revolve around transmuting an element to another and, sometimes, with secondary effects. <strong>Activable only during your turn</strong>.</>}
         </li>
         <li>
           <strong>Disruption:</strong>{' '}
           {language === 'ita'
-            ? 'effetto rapido che può essere attivato in risposta a un’altra azione, in qualsiasi momento, e se è un Chakra, direttamente dalla tua mano.'
-            : 'rapid effect that can be activated in response to another action, whenever you want and, if it’s a Chakra, directly from your hand.'}
+            ? <>effetto rapido che può essere attivato in risposta a un’altra azione, <strong>in qualsiasi momento</strong>, e se è un Chakra, direttamente dalla tua mano.</>
+            : <>rapid effect that can be activated <strong>in response to another action</strong>, <strong>whenever you want</strong> and, if it’s a Chakra, directly from your hand.</>}
         </li>
         <li>
           <strong>Shell:</strong>{' '}
           {language === 'ita'
-            ? 'effetto passivo che protegge l’entità da altri effetti e può avere effetti secondari.'
-            : 'passive type of effects that protects the entity against other effects and may have some secondary effects.'}
+            ? <>effetto passivo che protegge l’entità da altri effetti e può avere effetti secondari.</>
+            : <>passive type of effects that protects the entity against other effects and may have some secondary effects.</>}
         </li>
         <li>
           <strong>Haste:</strong>{' '}
           {language === 'ita'
-            ? 'effetto passivo che consente all’entità di attaccare tutte le entità avversarie durante la fase di battaglia, talvolta con effetti secondari.'
-            : 'passive type of effects that grant the entity the ability to attack all of your opponents entities during your battle phase, sometimes with secondary effects.'}
+            ? <>effetto passivo che consente all’entità di attaccare tutte le entità avversarie durante la fase di battaglia, talvolta con effetti secondari.</>
+            : <>passive type of effects that grant the entity the ability to attack all of your opponents entities during your battle phase, sometimes with secondary effects.</>}
         </li>
         <li>
           <strong>Unseal:</strong>{' '}
           {language === 'ita'
-            ? 'effetto passivo utilizzabile solo quando il Chakra si trova nel Diyu. Una volta usato, il Chakra torna direttamente nel mazzo.'
-            : 'passive type of effects that is usable only when the Chakra is in the Diyu. Once you use it, the Chakra goes straight into the deck.'}
+            ? <>effetto passivo utilizzabile solo quando il Chakra si trova nel Diyu. Una volta usato, il Chakra torna direttamente nel mazzo.</>
+            : <>passive type of effects that is usable only when the Chakra is in the Diyu. Once you use it, the Chakra goes straight into the deck.</>}
         </li>
         <li>
           <strong>Entrance:</strong>{' '}
           {language === 'ita'
-            ? 'effetto passivo che si attiva quando una carta entra nel Dominio.'
-            : 'passive type of effects that triggers when a card enters the Domain.'}
+            ? <>effetto passivo che si attiva quando una carta entra nel Dominio.</>
+            : <>passive type of effects that triggers when a card enters the Domain.</>}
         </li>
         <li>
           <strong>Lineage:</strong>{' '}
           {language === 'ita'
-            ? 'effetto speciale che viene ereditato dalle entità impilate sull’entità che lo possiede. Una volta ereditato, agisce come uno "Skill".'
-            : 'special type of effects that it’s inherited to the entities stacked on the entity that carries it. Once it’s inherited, it acts as a “Skill”.'}
+            ? <>effetto speciale che viene ereditato dalle entità impilate sull’entità che lo possiede. Una volta ereditato, agisce come uno <strong>"Skill"</strong>.</>
+            : <>special type of effects that it’s inherited to the entities stacked on the entity that carries it. Once it’s inherited, it acts as a <strong>“Skill”</strong>.</>}
         </li>
       </ul>
 
+      <div className={styles.dotDivider}>•</div>
+
       <p>
         {language === 'ita'
-          ? 'I Chakra funzionano in modo simile alle Entità ma hanno una caratteristica unica: possono essere posizionati coperti in zone specifiche, il che influisce su quando possono essere attivati:'
-          : 'Chakras function similarly to Entities but have a unique feature: they can be placed face-down in specific zones, which affects when they can be activated:'}
+          ? <>I <strong>Chakra</strong> funzionano in modo simile alle Entità ma hanno una caratteristica unica: possono essere <strong>posizionati coperti</strong> in zone specifiche, il che influisce su quando possono essere attivati:</>
+          : <>Chakras function similarly to Entities but have a unique feature: they can be <strong>placed face-down</strong> in specific zones, which affects when they can be activated:</>}
       </p>
 
       <ul style={{ listStyleType: 'disc', marginLeft: '1.2rem', paddingLeft: 0 }}>
         <li>
           {language === 'ita'
-            ? 'Durante il tuo turno: se il Chakra è nella tua mano o è stato sul campo per almeno due turni.'
-            : 'During your turn: If the Chakra is in your hand or has been on the field for at least two turns.'}
+            ? <><strong>Durante il tuo turno</strong>: se il Chakra è nella tua mano o è stato sul campo per almeno due turni.</>
+            : <><strong>During your turn</strong>: If the Chakra is in your hand or has been on the field for at least two turns.</>}
         </li>
         <li>
           {language === 'ita'
-            ? 'Durante il turno dell’avversario: se il Chakra è già sul campo.'
-            : 'During the opponent’s turn: If the Chakra is already on the field.'}
+            ? <><strong>Durante il turno dell’avversario</strong>: se il Chakra è già sul campo.</>
+            : <><strong>During the opponent’s turn</strong>: If the Chakra is already on the field.</>}
         </li>
       </ul>
 
       <p>
         {language === 'ita'
-          ? 'Inoltre, i Chakra posizionati possono essere attivati in qualsiasi fase del turno di entrambi i giocatori.'
-          : 'Additionally, placed Chakras can be activated during any phase of either player\'s turn.'}
+          ? <>Inoltre, i Chakra posizionati possono essere <strong>attivati in qualsiasi fase</strong> del turno di entrambi i giocatori.</>
+          : <>Additionally, placed Chakras can be <strong>activated during any phase</strong> of either player's turn.</>}
       </p>
 
       <p>
         {language === 'ita'
-          ? 'Eccezione: i Chakra con l’effetto Interruption possono essere attivati direttamente dalla tua mano, anche durante il turno dell’avversario!'
-          : 'Exception: Chakras with the Interruption effect can be activated directly from your hand, even during the opponent’s turn!'}
+          ? <>Eccezione: i Chakra con l’effetto <strong>Interruption</strong> possono essere attivati direttamente dalla tua mano, anche durante il turno dell’avversario!</>
+          : <>Exception: Chakras with the <strong>Interruption</strong> effect can be activated directly from your hand, even during the opponent’s turn!</>}
       </p>
 
       <p>
         {language === 'ita'
-          ? 'Ricorda: se tutti gli slot Nexus sono occupati, non puoi attivare altri Chakra!'
-          : 'Remember: if all Nexus slots are occupied, you cannot activate any more Chakras!'}
+          ? <>Ricorda: se tutti gli slot Nexus sono occupati, non puoi attivare altri <strong>Chakra</strong>!</>
+          : <>Remember: if all Nexus slots are occupied, you cannot activate any more <strong>Chakras</strong>!</>}
+      </p>
+
+      <div className={styles.dotDivider}>•</div>
+
+      <p>
+        {language === 'ita'
+          ? <>Gli effetti possono essere attivati solo <strong>durante la Main Phase o la Recovery Phase</strong>, con l’eccezione degli effetti <strong>Disruption</strong>, che come detto possono essere attivati anche nel turno dell’avversario.</>
+          : <>Effects can only be activated <strong>during the Main Phase or the Recovery Phase</strong>, with the exception of <strong>Disruption</strong>, which, as mentioned earlier, can be activated even during the opponent's turn.</>}
       </p>
 
       <p>
         {language === 'ita'
-          ? 'Gli effetti possono essere attivati solo durante la Main Phase o la Recovery Phase, con l’eccezione degli effetti Distruzione, che come detto possono essere attivati anche nel turno dell’avversario.'
-          : 'Effects can only be activated during the Main Phase or the Recovery Phase, with the exception of Disruption, which, as mentioned earlier, can be activated even during the opponent\'s turn.'}
+          ? <>Se un effetto di un <strong>Chakra</strong> posizionato viene attivato durante la Battle Phase, l’avversario deve terminare l’attacco in corso, con il nuovo effetto applicato.</>
+          : <>If an effect of a placed <strong>Chakra</strong> is activated during the Battle Phase, the opponent must finish their attack if they were in the process of attacking, with the new effect applied.</>}
       </p>
+
+      <div className={styles.dotDivider}>•</div>
 
       <p>
         {language === 'ita'
-          ? 'Se un effetto di un Chakra posizionato viene attivato durante la Battle Phase, l’avversario deve terminare l’attacco in corso.'
-          : 'If an effect of a placed Chakra is activated during the Battle Phase, the opponent must finish their attack if they were in the process of attacking.'}
+          ? <>Nel caso di un effetto <strong>Disruption</strong>: se la carta è posizionata, si applica la regola precedente; se invece è in mano, l’avversario può scegliere di annullare l’attacco.</>
+          : <>In the case of a <strong>Disruption</strong>: if the card is placed, the situation described above applies; however, if the card is in the hand, the opponent can choose to cancel the attack.</>}
       </p>
 
-      <p>
-        {language === 'ita'
-          ? 'Nel caso di un effetto Distruzione: se la carta è posizionata, si applica la regola precedente; se invece è in mano, l’avversario può scegliere di annullare l’attacco.'
-          : 'In the case of an Disruption: if the card is placed, the situation described above applies; however, if the card is in the hand, the opponent can choose to cancel the attack.'}
-      </p>
       <hr style={{ width: '80%', margin: '2rem auto', border: '1px solid #ccc' }} />
     </div>
   );
