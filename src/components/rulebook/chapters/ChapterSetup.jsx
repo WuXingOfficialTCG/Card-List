7bf7939ebd1e0780cdb68a0872e2f7f1fb983ea8
WuXingOfficialTCG <wuxingofficial@gmail.com>
2025-06-11 11:09:25 +0200

    Update ChapterSetup.jsx

--- src/components/rulebook/chapters/ChapterSetup.jsx
+++ src/components/rulebook/chapters/ChapterSetup.jsx
@@ -8,58 +8,58 @@ const ChapterSetup = ({ language }) => {
       {language === 'ita' ? (
         <>
           <p>
-            Per giocare a Wu Xing, tutto ciò di cui hai bisogno è un mazzo di carte interamente scelto da te e qualche segnalino per tenere traccia del tuo Qi.
+            Per giocare a Wu Xing, tutto ciò di cui hai bisogno è <strong>un mazzo di carte</strong> interamente scelto da te e <strong>qualche segnalino</strong> per tenere traccia del tuo Qi.
           </p>
           <p>
-            Puoi costruire liberamente il tuo mazzo usando qualsiasi carta tu voglia, sia dal Mazzo Alpha che da future espansioni, per strategie ancora più avanzate!
+            Puoi costruire liberamente il tuo mazzo usando qualsiasi carta tu voglia, sia dal <strong>Mazzo Alpha</strong> che da <strong>future espansioni</strong>, per strategie ancora più avanzate!
           </p>
-          
+
           <div className={styles.dotDivider}>•</div>
-          
+
           <p>Per iniziare una partita a Wu Xing, avrai bisogno di:</p>
           <ul>
-            <li>Deck: Deve contenere almeno 30 carte, fino a un massimo di 40.</li>
-            <li>Dadi: Ogni giocatore deve avere 5 dadi Qi.</li>
+            <li><strong>Deck</strong>: Deve contenere almeno <strong>30 carte</strong>, fino a un massimo di <strong>40</strong>.</li>
+            <li><strong>Dadi</strong>: Ogni giocatore deve avere 5 dadi Qi.</li>
           </ul>
           <p>Per un'esperienza più immersiva, puoi anche usare:</p>
           <ul>
-            <li>Playmat: Per organizzare meglio il campo di gioco.</li>
-            <li>Dado PM (d12): Utile per tenere traccia dei tuoi Punti Marma.</li>
+            <li><strong>Playmat</strong>: Per organizzare meglio il campo di gioco.</li>
+            <li><strong>Dado PM (d12)</strong>: Utile per tenere traccia dei tuoi Punti Marma.</li>
           </ul>
           <p>
-            Una volta che entrambi i giocatori hanno completato il setup, la partita può cominciare! Ogni giocatore inizia con 12 Punti Marma (PM), che rappresentano la loro vita.
+            Una volta che entrambi i giocatori hanno completato il setup, la partita può cominciare! Ogni giocatore inizia con <strong>12 Punti Marma (PM)</strong>, che rappresentano la loro vita.
           </p>
           <p>
-            Se i PM arrivano a zero, il giocatore perde. Dopo aver mescolato il proprio mazzo, ogni giocatore pesca 4 carte.
+            Se i PM arrivano a zero, il giocatore perde. Dopo aver mescolato il proprio mazzo, ogni giocatore <strong>pesca 4 carte</strong>.
           </p>
           <p>Ora sei pronto a giocare!</p>
         </>
       ) : (
         <>
           <p>
-            To play Wu Xing, all you need is a deck of cards entirely chosen by YOU and a few tokens to keep track of your Qi.
+            To play Wu Xing, <strong>all you need is a deck of cards</strong> entirely chosen by YOU and <strong>a few tokens</strong> to keep track of your Qi.
           </p>
           <p>
-            You can freely build your deck using any cards you want, whether from Alpha Set or future Expansions,  for even more advanced strategies!
+            You can freely build your deck using any cards you want, whether from <strong>Alpha Set or future Expansions</strong>, for even more advanced strategies!
           </p>
 
           <div className={styles.dotDivider}>•</div>
-          
+
           <p>To start a Wu Xing match, you will need:</p>
           <ul>
-            <li>Deck: Must contain at least 30 cards, up to a maximum of 40.</li>
-            <li>Dice: Each player must have 5 Qi dice.</li>
+            <li><strong>Deck</strong>: Must contain at least <strong>30 cards</strong>, up to a maximum of <strong>40</strong>.</li>
+            <li><strong>Dice</strong>: Each player must have 5 Qi dice.</li>
           </ul>
           <p>For a more immersive experience, you can also use:</p>
           <ul>
-            <li>Playmat: To better organize the playing field.</li>
-            <li>MP Die (d12): Useful for tracking your Marma Points.</li>
+            <li><strong>Playmat</strong>: To better organize the playing field.</li>
+            <li><strong>MP Die (d12)</strong>: Useful for tracking your Marma Points.</li>
           </ul>
           <p>
-            Once both players have completed their setup, the match can begin! Each player starts with 12 Marma Points (MP), representing their life.
+            Once both players have completed their setup, the match can begin! Each player starts with <strong>12 Marma Points (MP)</strong>, representing their life.
           </p>
           <p>
-            If MP reaches zero, the player loses. After shuffling their deck, each player draws 4 cards.
+            If MP reaches zero, the player loses. After shuffling their deck, each player <strong>draws 4 cards</strong>.
           </p>
           <p>Now you're ready to play!</p>
         </>
